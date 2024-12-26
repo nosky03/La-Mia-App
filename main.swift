c756a11ea18b5a57d4ead71db81d69fcf401f1ef
noskybarby <38571418-noskybarby@users.noreply.replit.com>
2024-12-26 15:02:20 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+import PlaygroundSupport
+
+struct ContentView: View {
+    var body: some View {
+      Color.white
+          .edgeIgnoringSafeArea(.all)
+    }
+}
+
+PlaygroundPage.current.setLiveView(ContentView())
